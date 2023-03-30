6e0dbd32b906e5194789025f8d5105ee6138fcbf
Jean Luc <jlah@oxo.mu>
2023-03-30 18:17:22 +0400

    test

---
+++ week6/day1/ex/main.js
@@ -0,0 +1,12 @@
+const count = banner.querySelector('countdown')
+setInterval(() => {
+    
+}, 2000);
+
+
+// setInterval(myTimer, 1000);
+
+// function myTimer() {
+//   const count = new count();
+//   document.querySelector('countdown').innerHTML = count.toLocaleTimeString();
+// }
